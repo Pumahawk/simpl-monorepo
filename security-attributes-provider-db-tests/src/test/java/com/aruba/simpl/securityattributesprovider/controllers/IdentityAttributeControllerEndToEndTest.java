aef0cd20dbba33522221cc75fc2a01a0336190ad
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-07-30 15:13:27 +0200

    aggiunta api addParticipantType, creati unit tests

--- security-attributes-provider-db-tests/src/test/java/com/aruba/simpl/securityattributesprovider/controllers/IdentityAttributeControllerEndToEndTest.java
+++ security-attributes-provider-db-tests/src/test/java/com/aruba/simpl/securityattributesprovider/controllers/IdentityAttributeControllerEndToEndTest.java
@@ -5,12 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.aruba.simpl.common.model.enums.ParticipantType;
 import com.aruba.simpl.common.test.WithMockSecurityContext;
 import com.aruba.simpl.securityattributesprovider.model.entities.IdentityAttribute;
 import com.aruba.simpl.securityattributesprovider.model.repositories.IdentityAttributeRepository;
 import com.aruba.simpl.securityattributesprovider.utils.TransactionalUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -77,6 +79,67 @@ public class IdentityAttributeControllerEndToEndTest extends EndToEndTest {
             assertThat(val1.isAssignableToRoles()).isTrue();
             assertThat(val2.isAssignableToRoles()).isTrue();
             assertThat(val3.isAssignableToRoles()).isFalse();
+        });
+    }
+
+    @Test
+    @WithMockSecurityContext(roles = "IATTR_M")
+    public void addParticipantType() throws Exception {
+
+        var ids = new LinkedList<String>();
+
+        tr.transactional(() -> {
+            var val1 = a(IdentityAttribute.class);
+            var val2 = a(IdentityAttribute.class);
+            var val3 = a(IdentityAttribute.class);
+            var val4 = a(IdentityAttribute.class);
+
+            val1.setId(null);
+            val1.getParticipantTypes().clear();
+            val1.getParticipantTypes().add(ParticipantType.CONSUMER);
+            val2.setId(null);
+            val2.getParticipantTypes().clear();
+            val2.getParticipantTypes().addAll(Arrays.asList(ParticipantType.CONSUMER, ParticipantType.DATA_PROVIDER));
+            val3.setId(null);
+            val3.getParticipantTypes().clear();
+            val4.setId(null);
+            val4.getParticipantTypes().clear();
+
+            repository.save(val1);
+            repository.save(val2);
+            repository.save(val3);
+            repository.save(val4);
+
+            manager.flush();
+            manager.clear();
+
+            ids.add(val1.getId().toString());
+            ids.add(val2.getId().toString());
+            ids.add(val3.getId().toString());
+            ids.add(val4.getId().toString());
+        });
+
+        var idsUpdate = ids.subList(0, 3);
+
+        mockMvc.perform(put("/identity-attribute/addParticipantType/" + ParticipantType.DATA_PROVIDER)
+                        .header("Content-type", MediaType.APPLICATION_JSON_VALUE)
+                        .content(new ObjectMapper().writeValueAsString(idsUpdate)))
+                .andExpect(status().is(200));
+
+        tr.transactional(() -> {
+            var val1 = repository.getReferenceById(UUID.fromString(ids.get(0)));
+            var val2 = repository.getReferenceById(UUID.fromString(ids.get(1)));
+            var val3 = repository.getReferenceById(UUID.fromString(ids.get(2)));
+            var val4 = repository.getReferenceById(UUID.fromString(ids.get(3)));
+
+            assertThat(val1.getParticipantTypes()
+                            .containsAll(Arrays.asList(ParticipantType.CONSUMER, ParticipantType.DATA_PROVIDER)))
+                    .isTrue();
+            assertThat(val2.getParticipantTypes().contains(ParticipantType.DATA_PROVIDER))
+                    .isTrue();
+            assertThat(val3.getParticipantTypes().contains(ParticipantType.DATA_PROVIDER))
+                    .isTrue();
+            assertThat(val4.getParticipantTypes().isEmpty()).isTrue();
         });
     }
 }
