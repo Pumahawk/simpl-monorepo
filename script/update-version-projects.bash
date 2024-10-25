bb570423a3767d24c4701086c29315888900e6a8
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-10-25 14:55:35 +0200

    add pipeline script commit

--- script/update-version-projects.bash
+++ script/update-version-projects.bash
@@ -44,8 +44,10 @@ function main() {
 			GIT_DIR="$line";
 			PROJECT_DIR="$(dirname "$GIT_DIR")"
 			git_fetch_remote "$GIT_DIR";
+			git_stash_all "$GIT_DIR";
 			git_checkout_new_branch "$GIT_DIR";
 			update_version_pipeline "$PROJECT_DIR";
+			git_commit "$GIT_DIR";
 		done;
 	)
 }
@@ -62,11 +64,24 @@ function git_checkout_new_branch() {
 	GIT_DIR="$GIT_DIR" git checkout -b "$GIT_BRANCH_NAME";
 }
 
+function git_stash_all() {
+	GIT_DIR="$1";
+	echo "Stash not yet implemented";
+}
+
 function update_version_pipeline() {
 	PROJECT_DIR="$1";
 	PIPELINE_FILE="$(find "$PROJECT_DIR" -name pipeline.variables.sh)";
 	echo "Update versione pipeline. Project directory: $PROJECT_DIR, pipeline version: $PIPELINE_VERSION, pipeline file: $PIPELINE_FILE";
 	sed -i 's/PROJECT_VERSION_NUMBER=.*/PROJECT_VERSION_NUMBER="'"$PIPELINE_VERSION"'"/' "$PIPELINE_FILE"
+}
+
+function git_commit() {
+	GIT_DIR="$1";
+	GIT_MESSAGE="Update pipeline version";
+	echo "Git add all and commit. Message: $GIT_MESSAGE";
+	GIT_DIR="$GIT_DIR" git add -A;
+	GIT_DIR="$GIT_DIR" git commit -m "$GIT_MESSAGE";
 }
 
 main "$@";
