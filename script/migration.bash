c84ab5912fd606ccf876ad77c5bc2ec90e75e799
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-05-21 18:10:58 +0200

    update migration script

--- script/migration.bash
+++ script/migration.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# From securityattributesprovider
+# To onboarding
+
 TMP_TABLE="tmp_stage";
 CSV_PATH="/tmp/identity_attributes_participant_types.csv";
 
@@ -43,19 +46,65 @@ EOF
 )";
 
 function main() {
+
+	while [ -n "$1" ]; do
+		case "$1" in
+			--db-host)
+				local db_host="$2"
+				shift;
+				;;
+			--from-db)
+				local from_db="$2"
+				shift;
+				;;
+			--from-user)
+				local from_user="$2"
+				shift;
+				;;
+			--from-password)
+				local from_password="$2"
+				shift;
+				;;
+			--to-db)
+				local to_db="$2"
+				shift;
+				;;
+			--to-user)
+				local to_user="$2"
+				shift;
+				;;
+			--to-password)
+				local to_password="$2"
+				shift;
+				;;
+			*)
+				;;
+		esac;
+		shift;
+	done;
+
+	local from_db="${from_db-"securityattributesprovider"}"
+	local to_db="${to_db-"onboarding"}"
+
+	[[ -z "${db_host}" ]] && { log "Missing parameter --db-host"; exit 1; }
+	[[ -z "${from_user}" ]] && { log "Missing parameter --from-user"; exit 1; }
+	[[ -z "${from_password}" ]] && { log "Missing parameter --from-password"; exit 1; }
+	[[ -z "${to_user}" ]] && { log "Missing parameter --to-user"; exit 1; }
+	[[ -z "${to_password}" ]] && { log "Missing parameter --to-password"; exit 1; }
+
 	log "Extraction data from securityattributesprovider to $CSV_PATH"
-	local db="securityattributesprovider"
-	PGDATABASE="${db}" \
-		PGUSER="${db}" \
-		PGPASSWORD="${db}" \
-		psql "$@" -f <( echo "$SQL_SAP" )
+	PGHOST="${db_host}" \
+		PGDATABASE="${from_db}" \
+		PGUSER="${from_user}" \
+		PGPASSWORD="${from_password}" \
+		psql "$@" -f <( echo "$SQL_SAP" ) || { log "Unable to retrieve data from sap"; exit 1; }
 
 	log "Load data to onboarding from $CSV_PATH"
-	local db="onboarding"
-	PGDATABASE="${db}" \
-		PGUSER="${db}" \
-		PGPASSWORD="${db}" \
-		psql "$@" -f <(echo "$SQL_ONBOARDING")
+	PGHOST="${db_host}" \
+		PGDATABASE="${to_db}" \
+		PGUSER="${to_user}" \
+		PGPASSWORD="${to_password}" \
+		psql "$@" -f <(echo "$SQL_ONBOARDING") || { log "Unable to write data to onboarding"; exit 1; }
 }
 
 function log() {
