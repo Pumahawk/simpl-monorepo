49f390f4124261024d0580464473185572142bb4
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-07-02 14:43:31 +0200

    Update reset-dataspace script

--- script/reset-dataspace.sh
+++ script/reset-dataspace.sh
@@ -19,79 +19,97 @@ if [[ "$RESET_AUTHORITY_ENVIRONMENT_SKIP" != "1" ]]; then
 	sleep "$sleep_sec"
 fi
 
+# TYPE="${1?Insert type [participant, authority]}"
+# log "Type: $type"
+
+
 KUBECTL_CMD=(kubectl)
 if [[ -n $K_NAMESPACE ]]; then
 	KUBECTL_CMD+=("--namespace" "${K_NAMESPACE}")
 fi
 
 ALL_DATABASE=(
-	"authenticationprovider"
-	"ejbca"
-	"identityprovider"
-	"keycloak"
-	"onboarding"
-	"securityattributesprovider"
-	"usersroles"
+	"authenticationprovider:authenticationprovider:authenticationprovider"
+	"ejbca:ejbca:ejbca2123"
+	"identityprovider:identityprovider:identityprovider"
+	"keycloak:keycloak:keycloak"
+	"onboarding:onboarding:onboarding"
+	"securityattributesprovider:securityattributesprovider:securityattributesprovider"
+	"usersroles:usersroles:usersroles"
 )
 
 ALL_DEPLOYMENTS=(
-	"authentication-provider"
-	"ejbca-community-helm"
-	"identity-provider"
-	"onboarding"
-	"security-attributes-provider"
-	"tier1-gateway"
-	"tier2-gateway"
-	"users-roles"
+	"deployment/authentication-provider"
+	"deployment/ejbca-community-helm"
+	"deployment/identity-provider"
+	"deployment/onboarding"
+	"deployment/security-attributes-provider"
+	"deployment/tier1-gateway"
+	"deployment/tier2-gateway"
+	"deployment/users-roles"
+	"statefulset.apps/keycloak"
 )
 
 
 function main() {
 	LOG_CONTEXT=main
+
 	log  "Clean dataspace start."
 
-	LOG_CONTEXT="verifyEnvironment" verifyEnvironment || {
-		log "Invalid cluster status. Validation error"
-		return 1
-	}
+	# LOG_CONTEXT="verifyEnvironment" verifyEnvironment || {
+	# 	log "Invalid cluster status. Validation error"
+	# 	return 1
+	# }
 
-	LOG_CONTEXT="scaleAllTo0" scaleAllTo0 || {
-		log "Unable to scale all to 0"
-		return 1
-	}
-
-	LOG_CONTEXT="waitAllScaleTo0" waitAllScaleTo0 || {
-		log "Unable to waitAllScaleTo0"
-		return 1
-	}
-
-	LOG_CONTEXT="cleanDB" cleanDB || {
-		log "Unable to clean database"
-		return 1
-	}
-
+	# [[ "$TYPE" == "authority" ]] && \
 	LOG_CONTEXT="deleteEJBCASecrets" deleteEJBCASecrets || {
 		log "Unable to delate EJBCA Secrets"
 		log "Skip condition secrets"
 	}
 
-	LOG_CONTEXT="scaleAllTo1" scaleAllTo1 || {
-		log "Unable to scale all to 1"
-		return 1
+	LOG_CONTEXT="cleanDB" cleanDB || {
+		log "Unable to clean database"
+		# return 1
 	}
 
-	LOG_CONTEXT="waitAllScaleTo1" waitAllScaleTo1 || {
-		log "Unable to waitAllScaleTo1"
-		return 1
+	LOG_CONTEXT="scaleAllTo0" scaleAllTo0 || {
+		log "Unable to scale all to 0"
+		# return 1
+	}
+
+	log "Sleep 5 seconds"
+	sleep 5
+
+	# LOG_CONTEXT="waitAllScaleTo0" waitAllScaleTo0 || {
+	# 	log "Unable to waitAllScaleTo0"
+	# 	# return 1
+	# }
+
+	LOG_CONTEXT="scaleAllTo1" scaleAllTo1 || {
+		log "Unable to scale all to 1"
+		# return 1
+	}
+
+	# LOG_CONTEXT="waitAllScaleTo1" waitAllScaleTo1 || {
+	# 	log "Unable to waitAllScaleTo1"
+	# 	# return 1
+	# 	log "MANUALLY DELETE INIT JOB!!"
+	# }
+
+	LOG_CONTEXT="deleteInitJob" deleteInitJob || {
+		log "Unable to delete init job"
 	}
 
 }
 
 function cleanDB() {
 	log "Start Clean DB"
-	for db in "${ALL_DATABASE[@]}"; do
-		( dropdb "$db" && createdb -O "$db" "$db" ) || { log "Unable drop or create db $db"; return 1; }
-	done
+       for db_conn in "${ALL_DATABASE[@]}"; do
+	       local db="$(echo $db_conn | cut -d: -f1)"
+	       local user="$(echo $db_conn | cut -d: -f2)"
+	       local password="$(echo $db_conn | cut -d: -f3)"
+               ( PGPASSWORD="${password}" dropdb -f -U${user} "$db" && PGPASSWORD="${password}" createdb -U$user -O "$user" "$db" ) || { log "Unable drop or create db $db"; }
+       done
 	log "Success Clean Database"
 }
 
@@ -112,6 +130,8 @@ function verifyEnvironment() {
 	&& exitsAllExpectedDatabase
 }
 
+# Skip this part.
+# Script must work in authority and participant
 function isAuthorityDataspace() {
 	log "Verify isAuthorityDataspace"
 	log "Retrieve current namespace from onboarding deployment"
@@ -144,7 +164,7 @@ function verifyEJBCAStatus() {
 
 function hasAllDeployments() {
 	for dep in "${ALL_DEPLOYMENTS[@]}"; do
-		hasDeployment  "$dep" || { log "ERROR - Not found deployment $dep"; return 1; }
+		hasDeployment  "$dep" || { log "ERROR - Not found deployment $dep"; }
 	done
 }
 
@@ -157,10 +177,13 @@ function hasDeployment() {
 function exitsAllExpectedDatabase() {
 	log "Verify exitsAllExpectedDatabase"
 
-	for db in "${ALL_DATABASE[@]}"; do
-		if ! psql -tAc "select 1 from pg_database where datname = '$db'" | grep -q 1; then
+	for db_conn in "${ALL_DATABASE[@]}"; do
+	       local db="$(echo $db_conn | cut -d: -f1)"
+	       local user="$(echo $db_conn | cut -d: -f2)"
+	       local password="$(echo $db_conn | cut -d: -f3)"
+		if ! PGPASSWORD=$password psql -U$user -tAc "select 1 from pg_database where datname = '$db'" | grep -q 1; then
 			log "ERROR - Not found dabatase '$db'"
-			return 1
+			# return 1
 		fi
 	done
 }
@@ -168,14 +191,14 @@ function exitsAllExpectedDatabase() {
 function scaleAllTo1() {
 	for dep in "${ALL_DEPLOYMENTS[@]}"; do
 		log "Scale to 1 $dep"
-		kubectl_cmd scale --replicas 1 deployment "$dep" || { log "ERROR - Unable to scale project $dep"; return 1; }
+		kubectl_cmd scale --replicas 1 "$dep" || { log "ERROR - Unable to scale project $dep"; }
 	done
 }
 
 function scaleAllTo0() {
 	for dep in "${ALL_DEPLOYMENTS[@]}"; do
 		log "Scale to 0 $dep"
-		kubectl_cmd scale --replicas 0 deployment "$dep" || { log "ERROR - Unable to scale project $dep"; return 1; }
+		kubectl_cmd scale --replicas 0 "$dep" || { log "ERROR - Unable to scale project $dep"; }
 	done
 }
 
@@ -183,7 +206,7 @@ function waitAllScaleTo1() {
 	local cond="[1-9]/"
 	local seconds="5"
 	for dep in "${ALL_DEPLOYMENTS[@]}"; do
-		waitDeploymentStatus  "$dep" "$cond" "$seconds" || { log "ERROR - Unable to wait project $dep"; return 1; }
+		waitDeploymentStatus "$dep" "$cond" "$seconds" || { log "ERROR - Unable to wait project $dep"; }
 	done
 }
 
@@ -191,7 +214,7 @@ function waitAllScaleTo0() {
 	local cond="0/"
 	local seconds="5"
 	for dep in "${ALL_DEPLOYMENTS[@]}"; do
-		waitDeploymentStatus  "$dep" "$cond" "$seconds" || { log "ERROR - Unable to wait project $dep"; return 1; }
+		waitDeploymentStatus "$dep" "$cond" "$seconds" || { log "ERROR - Unable to wait project $dep"; }
 	done
 }
 
@@ -202,10 +225,10 @@ function waitDeploymentStatus() {
 
 	local dep_status=0
 	while [[ "$dep_status" != 1 ]]; do
-		local dep_output="$( kubectl_cmd get deployment --no-headers "$dep_name" )"
+		local dep_output="$( kubectl_cmd get --no-headers "$dep_name" )"
 		if [[ $? != 0 ]]; then
 			log "ERROR - unable to get deployment $dep_name"
-			return 1
+			# return 1
 		fi
 		dep_status="$(echo "$dep_output" | grep -E "$dep_exp_status" | wc -l )"
 		if [[ $dep_status != 1 ]]; then
@@ -215,8 +238,12 @@ function waitDeploymentStatus() {
 	done
 }
 
+function deleteInitJob() {
+	kubectl_cmd delete job init-authority-job || kubectl_cmd delete job init-participant-job
+}
+
 function kubectl_cmd() {
 	"${KUBECTL_CMD[@]}" "$@"
 }
 
-main
+main "$@"
