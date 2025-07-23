0b8ebf567eb372c942f4548a058c5c27365c079b
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-07-23 10:36:34 +0200

    Change output properties file

--- script/properties-find.bash
+++ script/properties-find.bash
@@ -3,7 +3,11 @@ function lookup() {
 	local properties="$(cat "$file" |
 	yq '.properties | to_entries[] | select(.value.type == "string" and .value.maxLength == null and  .value.format == null) | .key')";
 		if [ -n "$properties" ]; then
-			echo -e "$file\\n[\\n $properties \\n]\\n" 
+			# echo -e "$file\\n[\\n $properties \\n]\\n" 
+			echo "$properties" | while read line; do
+				grep -Hn "${line}:" "$file" | head -n1
+			done;
+
 		fi
 }
 
