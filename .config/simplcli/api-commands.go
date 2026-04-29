7ec38d40c18fcdcd26d43c364590bdab369a8f5a
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-04-29 18:06:57 +0200

    update

--- .config/simplcli/api-commands.go
+++ .config/simplcli/api-commands.go
@@ -5,38 +5,92 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"simplcli/internal/simpl"
+	"slices"
+	"strings"
 	"text/tabwriter"
 )
+
+type SearchFunc[T any, R any] func(page, size int, filter T) (R, error)
 
 var ApiKeypairsSearchCmd = command{
 	Name: "keypair:search",
 	Func: func(s ...string) int8 {
-
-		var page, size int
-
 		// Set search parameters with flags
 		search := &simpl.KeypairsSearch{}
-		fs := flag.NewFlagSet("", flag.ExitOnError)
-		fs.StringVar(&search.Name, "name", "", "")
-		fs.IntVar(&page, "page", 0, "")
-		fs.IntVar(&size, "size", 10, "")
-		fs.Parse(s)
-
 		c := simpl.NewAuthorityClient()
-		r, err := c.Keypairs(page, size, search)
-		if err != nil {
-			log.Fatalf("unable to make keypair: %s", err)
-		}
-
-		fmt.Printf("page: %d\n", page)
-		fmt.Printf("size: %d\n", size)
-		w := tabwriter.NewWriter(os.Stdout, 6, 2, 2, ' ', 0)
-		fmt.Fprintf(w, "%s\t%s\n", "Id", "Name")
-		for _, k := range r.Items {
-			fmt.Fprintf(w, "%s\t%s\n", k.Id, k.Name)
-		}
-		w.Flush()
-		return 0
+		return searchBaseCmd(search, c.Keypairs, s)
 	},
+}
+
+func searchBaseCmd[T any, R any](search T, searchFunc SearchFunc[T, R], s []string) int8 {
+	// Set search parameters with flags
+	fs := flag.NewFlagSet("", flag.ExitOnError)
+	page, size := searchFlag(fs, "sh", search)
+	fieldsFlag := fs.String("fields", "", "")
+	fs.Parse(s)
+
+	fieldsS := *fieldsFlag
+	var fieldsArgs []string
+	if fieldsS != "" {
+		fieldsArgs = strings.Split(*fieldsFlag, ",")
+	}
+
+	r, err := searchFunc(*page, *size, search)
+	if err != nil {
+		log.Fatalf("unable to make search: %s", err)
+	}
+
+	fmt.Printf("page: %d\n", *page)
+	fmt.Printf("size: %d\n", *size)
+	w := tabwriter.NewWriter(os.Stdout, 6, 2, 2, ' ', 0)
+	rf := reflect.ValueOf(r).Elem().FieldByName("Items")
+	rt := rf.Type().Elem()
+	var fields []string
+	for i := 0; i < rt.NumField(); i++ {
+		if len(fieldsArgs) == 0 || slices.Contains(fieldsArgs, rt.Field(i).Name) {
+			fields = append(fields, rt.Field(i).Name)
+		}
+	}
+	w.Write([]byte(strings.Join(fields, "\t") + "\n"))
+	for i := 0; i < rf.Len(); i++ {
+		item := rf.Index(i)
+		var itemsS []string
+		for _, fieldName := range fields {
+			fmts := "%v"
+			itemField := item.FieldByName(fieldName)
+			itemValue := itemField.Interface()
+			if itemField.Kind() == reflect.String {
+				fmts = "%q"
+			}
+			itemsS = append(itemsS, fmt.Sprintf(fmts, itemValue))
+		}
+		w.Write([]byte(strings.Join(itemsS, "\t") + "\n"))
+	}
+	w.Flush()
+	return 0
+}
+
+func searchFlag(fl *flag.FlagSet, prefix string, search any) (*int, *int) {
+	rf := reflect.ValueOf(search)
+	if rf.Kind() == reflect.Pointer {
+		rf = rf.Elem()
+	}
+	rt := rf.Type()
+	for i := range rf.NumField() {
+		f := rf.Field(i)
+		name := rt.Field(i).Tag.Get("search")
+		if name == "" {
+			name = rt.Field(i).Name
+		}
+		flagName := prefix + "-" + name
+		if prefix == "" {
+			flagName = name
+		}
+		fl.StringVar(f.Addr().Interface().(*string), flagName, "", "")
+	}
+	page := fl.Int("page", 0, "")
+	size := fl.Int("size", 10, "")
+	return page, size
 }
