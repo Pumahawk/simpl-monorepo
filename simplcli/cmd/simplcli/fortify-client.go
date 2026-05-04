df389733caa2070adb8bc5010e2bc6a781ae9bfe
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-05-04 02:47:37 +0200

    refactor client fortify configuration

---
+++ simplcli/cmd/simplcli/fortify-client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Pumahawk/simpl-monorepo/internal/fortify"
+)
+
+var fortifyClient = fortify.Client{
+	BaseUrl: "https://api.emea.fortify.com",
+	TokenFunc: func() (*fortify.AuthData, error) {
+		envtu := "FORTIFY_USER"
+		envtk := "FORTIFY_TOKEN"
+		u, _ := os.LookupEnv(envtu)
+		t, _ := os.LookupEnv(envtk)
+		if u == "" || t == "" {
+			return nil, fmt.Errorf("not found valid auth configuration, define env %q %q", envtu, envtk)
+		}
+		return &fortify.AuthData{Token: t, User: u}, nil
+	},
+}
--- simplcli/cmd/simplcli/gitlab-client.go
+++ simplcli/cmd/simplcli/gitlab-client.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/Pumahawk/simpl-monorepo/internal/fortify"
 	"github.com/Pumahawk/simpl-monorepo/internal/gitlab"
 )
 
@@ -13,19 +11,5 @@ var gitlabClient = gitlab.Client{
 	TokenFunc: func() (string, error) {
 		t, _ := os.LookupEnv("GITLAB_TOKEN")
 		return t, nil
-	},
-}
-
-var fortifyClient = fortify.Client{
-	BaseUrl: "https://api.emea.fortify.com",
-	TokenFunc: func() (*fortify.AuthData, error) {
-		envtu := "FORTIFY_USER"
-		envtk := "FORTIFY_TOKEN"
-		u, _ := os.LookupEnv(envtu)
-		t, _ := os.LookupEnv(envtk)
-		if u == "" || t == "" {
-			return nil, fmt.Errorf("not found valid auth configuration, define env %q %q", envtu, envtk)
-		}
-		return &fortify.AuthData{Token: t, User: u}, nil
 	},
 }
