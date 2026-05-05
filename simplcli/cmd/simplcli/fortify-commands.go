b0e43e887b4f07de03e6b014a1f63060b9328723
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-05-05 02:24:59 +0200

    create fortify false positive command

---
+++ simplcli/cmd/simplcli/fortify-commands.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/Pumahawk/simpl-monorepo/internal/cmd"
+)
+
+var FortifyFalsePositiveCmd = cmd.Command[int]{
+	Name: "false-positive",
+	Run: func(c *cmd.Command[int], args []string) (int, error) {
+		var releaseId string
+		fs := flag.NewFlagSet("", flag.ExitOnError)
+		fs.Parse(args)
+		releaseId = fs.Arg(0)
+
+		if releaseId == "" {
+			return 1, fmt.Errorf("missing argument releaseId")
+		}
+
+		if err := fortifyClient.MarkFalsePositive(releaseId); err != nil {
+			return 1, fmt.Errorf("fortify mark false positive releaseId=%q", releaseId)
+		}
+		return 0, nil
+	},
+}
