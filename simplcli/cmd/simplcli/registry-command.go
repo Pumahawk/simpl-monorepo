9d9af5b096797af457d233579d13542861e9523a
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-05-06 19:00:44 +0200

    refactor GitlabRegistryCmd

--- simplcli/cmd/simplcli/registry-command.go
+++ simplcli/cmd/simplcli/registry-command.go
@@ -3,30 +3,72 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"sync"
 
 	"github.com/Pumahawk/simpl-monorepo/internal/cmd"
 	"github.com/Pumahawk/simpl-monorepo/internal/gitlab"
 )
 
-var GitlabRegistryCmd = cmd.Command[any]{
+type GitlabRegistryItemModel struct {
+	Id          int    `json:"id"`
+	Project     string `json:"project"`
+	Name        string `json:"name"`
+	Version     string `json:"version"`
+	PackageType string `json:"package_type"`
+	CreatedAt   string `json:"created_at"`
+	CreatorId   int    `json:"creator_id"`
+}
+
+var GitlabRegistryCmd = cmd.Command[[]GitlabRegistryItemModel]{
 	Name: "registry:search",
-	Run: func(c *cmd.Command[any], args []string) (any, error) {
+	Run: func(c *cmd.Command[[]GitlabRegistryItemModel], args []string) ([]GitlabRegistryItemModel, error) {
 		search := &gitlab.SearchRegistry{}
 
 		fl := flag.NewFlagSet("", flag.ExitOnError)
 		structFlag(fl, search)
 		fl.Parse(args)
-		projectId := fl.Arg(0)
+		projectIds := prIdsDemux.demux(fl.Args())
 
-		if projectId == "" {
+		if len(projectIds) == 0 {
 			return nil, fmt.Errorf("missing project id")
 		}
 
-		r, err := gitlabClient.Registry(prIds.Get(projectId), search)
-		if err != nil {
-			return nil, fmt.Errorf("unable to retrieve registry, project %q: %w", projectId, err)
+		ch := make(chan *GitlabRegistryItemModel)
+		go func() {
+			defer close(ch)
+			wg := &sync.WaitGroup{}
+			wg.Go(func() {
+				for i := range projectIds {
+					projectName := projectIds[i]
+					projectId := prIds.Get(projectName)
+					r, err := gitlabClient.Registry(prIds.Get(projectId), search)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "unable to retrieve registry, project %q: %s\n", projectId, err)
+						return
+					}
+					for i := range r.Items {
+						item := &r.Items[i]
+						ch <- &GitlabRegistryItemModel{
+							Id:          item.Id,
+							Project:     projectName,
+							Name:        item.Name,
+							Version:     item.Version,
+							PackageType: item.PackageType,
+							CreatedAt:   item.CreatedAt,
+							CreatorId:   item.CreatorId,
+						}
+					}
+				}
+			})
+			wg.Wait()
+		}()
+
+		items := make([]GitlabRegistryItemModel, 0, 20*len(prIds))
+		for item := range ch {
+			items = append(items, *item)
 		}
 
-		return r, nil
+		return items, nil
 	},
 }
